5e696078deed3c8b0d7a075b2a2d9ffdd5b9c870
Danny Vander-Hyde <daniel.vanderhyde722@gmail.com>
2023-10-04 12:14:01 -0400

    Further details added to DRFPMI functions in ifo_configs.py and correcting figures

--- code/ifo_configs.py
+++ code/ifo_configs.py
@@ -54,18 +54,27 @@ def fpmi_freq_resp(freq, r_1, t_1, r_2, L, phi_0, P_in, OMEGA, low_pass=False):
         fpmi_resp = ((t_1**2 * r_2)/((t_1**2 + r_1**2)*r_2 - r_1))*(mich_freq_resp(freq, L, phi_0, P_in, OMEGA)/(1-r_1*r_2*np.exp(-1j*L*4.0*np.pi*freq/cee)))
     return fpmi_resp
 
-def PRG(L_rt, Finn):
+def PRG(L_rt, Finn, r_PRM, max=0):
     """
     POWER RECYCLING GAIN (@ optimal reflectivity)
     * Assuming a FPMI with symmetric arms *
     L_rt : Round trip loss
     Finn : Cavity finesse
     """
-    return np.pi/(2*Finn*L_rt*(1-((Finn*L_rt)/(2*np.pi))))
+    if max == 1:
+        G_PR = np.pi/(2*Finn*L_rt*(1-((Finn*L_rt)/(2*np.pi))))
+    else:
+        G_PR = (1-r_PRM**2)/(1-r_PRM*(1-(Finn/np.pi)*L_rt))**2
 
-def drfpmi_freq_resp(freq, G_PRC_opt, r_1, t_1, r_2, r_SRM, t_SRM, L, phi_0, P_in, OMEGA):
+
+    return G_PR
+
+def drfpmi_freq_resp(freq, G_PRC_opt, r_1, t_1, r_2, r_SRM, t_SRM, phi_SRC, L, phi_0, P_in, OMEGA):
     """
     DUAL RECYCLED FABRY PEROT MICHELSON FREQUENCY RESPONSE CALCULATOR
+
+   $$ \mathrm{H}_\mathrm{DRFPMI} = \mathrm{G}_\mathrm{PR} \mathrm{P}_\mathrm{in} L \Omega \bigg[ \frac{ t_1^2 r_2}{(t_1^2 + r_1^2)r_2 - r_1} \frac{t_\mathrm{SRM} t_1 e^{-i\phi_\mathrm{SRC}}}{1-r_1 r_\mathrm{SRM} e^{-i2\phi_\mathrm{SRC}}} \frac{e^{-i 2 \pi L f / c} \mathrm{sin}( 2 \pi f / c)}{ 2 \pi L f } \frac{\mathrm{sin}(\phi_0)}{1- [(r_1 - r_\mathrm{SRM} e^{-i2\phi_\mathrm{SRC}})/(1-r_1 r_\mathrm{SRM}e^{-i2\phi_\mathrm{SRC}})] r_2 e^{-i 4 \pi L f / c}} \bigg] $$ 
+
     freq: standard (gravitational wave) frequency [Hz]
     G_PRC_opt: maximum power recycling gain (optimal) [arb]
     r_1: ITM reflection coefficient [arb]
@@ -76,7 +85,10 @@ def drfpmi_freq_resp(freq, G_PRC_opt, r_1, t_1, r_2, r_SRM, t_SRM, L, phi_0, P_i
     L: Length of the Fabry-Perot arms [m]
     OMEGA: OPTICAL angular frequency [rad Hz]
     """
-    return ((t_1**2 * r_2)/((t_1**2 + r_1**2)*r_2 - r_1))*(G_PRC_opt*t_SRM*t_1/(1-r_1*r_SRM))*(P_in*L*OMEGA*np.exp((-1j*L*2.0*np.pi*freq)/cee)*np.sin((L*2.0*np.pi*freq)/cee)/(L*2.0*np.pi*freq))/(1-((r_1-r_SRM)/(1-r_1*r_SRM))*r_2*np.exp(-1j*L*4.0*np.pi*freq/cee))
+    r_SRC = (r_1 - r_SRM*np.exp(1j*2*phi_SRC))/(1 - r_1*r_SRM*np.exp(1j*2*phi_SRC))
+    t_SRC = t_1*t_SRM*np.exp(1j*phi_SRC)/(1 - r_1*r_SRM*np.exp(1j*2*phi_SRC))
+
+    return ((t_1**2 * r_2)/((t_1**2 + r_1**2)*r_2 - r_1))*G_PRC_opt*t_SRC*(P_in*L*OMEGA*np.exp((-1j*L*2.0*np.pi*freq)/cee)*np.sin((L*2.0*np.pi*freq)/cee)/(L*2.0*np.pi*freq))/(1-r_SRC*r_2*np.exp(-1j*L*4.0*np.pi*freq/cee))
 
 
 # Shot noise
