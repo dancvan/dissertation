2433dfe3a4f90d2d156fb9f26d11a870d7978b17
dancvan <dvande72hyde@hotmail.com>
2024-03-27 14:25:01 -0400

    Adding cylaplace cpu compute

---
+++ code/cylap/cpu/run.py
@@ -0,0 +1,197 @@
+import set_params
+import laplace
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('stylelib/surftex')
+#fig_exp_dirama = "../../../../dissertation/figs/ALGAAS/"
+from matplotlib import cm
+from matplotlib import rcParams
+import time
+
+---
+
+pdict = set_params.pdict
+pdict
+
+---
+
+pdict['res_exp']
+
+---
+
+# initialize coordinates
+coord_dict = laplace.init_coords(pdict) 
+
+# Imposing a square simulation space
+N = pdict['N'][0]
+
+# coord vecs
+rho = coord_dict['coords']['rho']
+z = coord_dict['coords']['z']
+inv_rho = coord_dict['coords']['invrho']
+
+#indices 
+irho = coord_dict['indices']['rho']
+iz = coord_dict['indices']['z']
+
+---
+
+fV = laplace.anal_sol(pdict)
+fV['V_anal'](coord_dict['indices']['z']*1e-4)
+
+---
+
+plt.plot(coord_dict['indices']['z']*1e-4,fV['V_anal'](coord_dict['indices']['z']*1e-4))
+
+--- 
+
+coord_dict['indices']['z']*1e-4
+
+---
+
+# intialize potential map, electric susceptibility, and LAMBD operator
+V = laplace.init_V(N)
+chi_e = laplace.init_V(N)
+chi_e_sub = pdict['optic']['sub_eps']-1
+chi_e_coat = pdict['optic']['coat_eps']-1
+LAMBD = laplace.build_lambd(irho, iz, N)
+
+---
+
+LAMBD
+
+---
+
+rho
+
+---
+
+# Translating (Dirichlet) boundary conditions to sim 
+
+# Initial value 
+
+## (For faster convergence) setting edge values
+
+### Edge locations
+r_max = (rho == max(rho))
+r_min = (rho == min(rho))
+z_max = (z == max(z))
+z_min = (z == min(z))
+
+# Boundary values 
+
+## Plate potentials
+fp = 'front_plate'
+bp = 'back_plate'
+loc_fp = np.logical_and(np.logical_and(rho>=pdict[fp]['hole_diam']/2,rho<=pdict[fp]['diam']/2),z == pdict[fp]['zpos'])
+loc_bp = np.logical_and(np.logical_and(rho>=pdict[bp]['hole_diam']/2,rho<=pdict[bp]['diam']/2),z == pdict[bp]['zpos'])
+#bc_fp = laplace.BC_dict([[pdict[fp]['hole_diam']/2, pdict[fp]['diam']/2], pdict[fp]['zpos']],pdict[fp]['voltage'],fp, LAMBD)
+#bc_bp = laplace.BC_dict([[pdict[bp]['hole_diam']/2, pdict[bp]['diam']/2], pdict[bp]['zpos']],pdict[fp]['voltage'],bp, LAMBD)
+
+# Exponential boundary conditions
+exp_rend  = rho==(max(rho)-pdict['res'][0])
+
+exp_z0 = z==(min(z)+pdict['res'][1])
+
+exp_zend = z==(max(z)-pdict['res'][1])
+
+# Setting sample dielectric
+loc_sub = np.logical_and(np.abs(z - pdict['optic']['z_com']) < np.round((pdict['optic']['thickness']/2),pdict['res_exp'][1]), (rho<np.round((pdict['optic']['diam']/2),pdict['res_exp'][0])))
+loc_coat1 = np.logical_and((z==np.round(pdict['loc_params']['front of optic']['z'],pdict['res_exp'][1])), (rho < np.round((pdict['optic']['diam']/2), pdict['res_exp'][0])))
+loc_coat2 = np.logical_and((z==np.round((pdict['loc_params']['front of optic']['z']-pdict['res'][1]),pdict['res_exp'][1])), (rho < (np.round(pdict['optic']['diam']/2, pdict['res_exp'][0]))))
+
+---
+
+## Initialize BCs
+
+#### Set susceptibility
+chi_e[loc_sub] = chi_e_sub
+chi_e[loc_coat1] = chi_e_coat
+chi_e[loc_coat2] = chi_e_coat
+
+## Electro-static conditions
+
+### Electrode plates
+V[loc_fp] = pdict[fp]['voltage']
+V[loc_bp] = pdict[bp]['voltage']
+
+#### Boundary values
+
+#### Edge for faster convergance
+V[z_min] = pdict['back_plate']['voltage']
+V[z_max] = pdict['front_plate']['voltage']
+V[r_max] = np.interp(np.arange(0,pdict['N'][0]), np.array([0,pdict['N'][0]-1]), np.array([pdict['back_plate']['voltage'], pdict['front_plate']['voltage']]))
+
+### Exponential (Dirichlet) boundary conditions
+V_exp = lambda V_0, R_0, V , R : V_0 + np.exp(-R/R_0)*(V - V_0) 
+V_char = 0
+R_char = 1.0
+
+#rho=rho_max
+V[r_max] = V_exp(V_char, R_char, V[exp_rend], pdict['res'][0])
+#z=z_min
+V[z_min] = V_exp(V_char, R_char, V[exp_z0], pdict['res'][1])
+#z=z_max
+V[z_max] = V_exp(V_char, R_char, V[exp_zend], pdict['res'][1])
+
+---
+
+#### Build operators
+lap = laplace.build_lap(pdict, LAMBD, irho)
+grad = laplace.build_grad(pdict, LAMBD)
+disp = laplace.build_disp(pdict, LAMBD)
+LAP = laplace.build_LAP(pdict, coord_dict, lap, grad, disp, chi_e)
+
+---
+
+t = time.time() 
+#### run sim
+for itr in range(0, pdict['iters']):
+    
+    V = V + (pdict['res'][0]*pdict['res'][0]*pdict['iter_step']*LAP.dot(V))
+    
+    ## Re-applying exponential condition
+    #rho=rho_max
+    V[r_max] = V_exp(V_char, R_char, V[exp_rend], pdict['res'][0])
+    #z=z_min
+    V[z_min] = V_exp(V_char, R_char, V[exp_z0], pdict['res'][1])
+    #z=z_max
+    V[z_max] = V_exp(V_char, R_char, V[exp_zend], pdict['res'][1])
+    
+    ### Re-apply Electro-static condition
+    V[loc_fp] = pdict[fp]['voltage']
+    V[loc_bp] = pdict[bp]['voltage']
+elapsed = time.time() - t
+print(elapsed)
+
+---
+
+fig = plt.figure(figsize = (18.5,21))
+ax = plt.axes(projection='3d') 
+surf = ax.plot_surface(rho.reshape(N,N), z.reshape(N,N),V.reshape(N,N),rstride=1,cstride=1,cmap=cm.inferno,alpha=1,linewidth=10,rasterized=True)
+fig.tight_layout()
+ax.view_init(20,210)
+ax.set_xlabel('r [m]')
+ax.set_ylabel('z [m]')
+ax.set_zlabel('[V]')
+fig.colorbar(surf, shrink=0.4, aspect=20, pad=-0.025)
+axes_width = fig.get_size_inches()[1]*(fig.subplotpars.right-fig.subplotpars.left)
+right =1.095
+left =-.15
+fig.subplots_adjust(left=left,right=right) 
+fig.set_size_inches((fig.get_size_inches()[0],axes_width/(right-left)))
+ax.tick_params(axis='both', pad=15)
+axes_height = fig.get_size_inches()[1]*(fig.subplotpars.top-fig.subplotpars.bottom)
+top = 1.15
+bottom=-.09
+fig.subplots_adjust(top=top,bottom=bottom)             
+fig.set_size_inches((fig.get_size_inches()[0],axes_height/(top-bottom)))
+#fig.savefig(fig_exp_dir + 'assembly1_sim.pdf', dpi=300, format='pdf')
+
+---
+
+# Plotting potential and field profiles
+laplace.pltxsect(pdict['loc_params']['halfway out on optic'], coord_dict, V)
+plt.plot(coord_dict['indices']['z']*1e-4,fV['V_anal'](coord_dict['indices']['z']*1e-4))
+
+## Comparison to analytical solution
---
+++ code/cylap/cpu/set_params.py
@@ -0,0 +1,226 @@
+import numpy as np
+## Setting parameters
+pdict ={
+    'coords' : 'cylindrical'             ,                        # coordinate system chosen for simulation box
+    'assembly' : 1                       ,                        # Establish plate geometry / location and voltage based on assembly 
+    'origin' : np.array([0,0])           ,                        # Origin of the simulation space / map
+    'size' : np.array([.04, .04])        ,                        # Size of simulation box [m] 
+    'res' : np.array([1,1])*1e-4         ,                        # relative resolution [coord1, coord2]
+    'iters' : 100000                     ,                        # total number of time iterations
+    'iter_step' : 0.1                    ,                        # time step
+    'expbc' : False                      ,                        # Exponential boundary conditions?
+    'bitres' : 'float32'                 ,                        # matrix element data type ('float32' vs 'float64')
+    'in2m' : .0254                       ,                        # frequently used conversion
+    'torch': False
+}
+    
+pdict['res_exp'] = np.abs(np.log10(pdict['res'])).astype('int')
+pdict['aspect'] = pdict['size'][0] == pdict['size'][1]
+pdict['N'] = (pdict['size']*(1/pdict['res']) + 1).astype('int')   # number of points sampled 1 dimension of simulation box
+
+# Sample parameters
+pdict['optic'] =  {
+        "diam" : 1.0*pdict['in2m'],
+        "thickness" : .25*pdict['in2m'], 
+        "z_com" : pdict['size'][1]/2,
+        "sub_eps" : 3.82,                                        # dielectric constant for substrate (fused silica)
+        "coat_eps" : 13.436,                                     # dielectric constant for coating material (AlGaAs / GaAs)
+        "coat_thickness" : 9.5e-6     
+}
+
+pdict['optic']['sub_thickness'] = pdict['optic']['thickness'] - pdict['optic']['coat_thickness']
+
+# CHOOSING ASSEMBLY CONFIGURATION
+    # Parameters established to characterize the assembly configurations:
+        # Front and back plate dimensions (usually disk diameters)
+        # Central aperture diameter
+        # Plate positioning along the beam axis with respect to simulation size center
+        # Maximum AC voltage sent on respective plates
+
+maxhva_settings = {
+        "SVR350" : 210,                                         # [Vpk]
+        "TREK2220" : 220,                                       # [Vpk]
+        "TREK5/80" : 1000,                                      # [Vpk]
+        "TREK10/10B-HS" : 1040                                  # [Vpk]
+} 
+
+if pdict['assembly'] == 0 or pdict['assembly'] == 1:
+    # Setting front and back plate params (including spacing between them and voltage on respective plates)
+    # This assembly has an assortment of 3d printed spacer components
+    pdict['HVA'] = "SVR350"
+    pdict['mount_zdims'] = {
+        "back_ring" : 1e-3,                                    # +/- 2e-4 [m]
+        "sample_holder" : 9e-3,                                # +/- 2e-4 [m]
+        "electrode_brace": 3e-3 ,                              # +/- 2e-4 [m]
+        "electrode_backing": 2e-3                              # +/- 2e-4 [m]
+    }
+    pdict['front_plate'] = {
+        "diam" : 3* pdict['in2m'],                             # diameter of plate [m]
+        "hole_diam" : 3e-3,                                    # aperture diameter [m]
+        "thickness" : 1.5e-3,
+        "zpos" : pdict['size'][1]/2 + pdict['mount_zdims']["sample_holder"]/2,         # location of plate surface (com) [m]
+        "voltage" : maxhva_settings[pdict['HVA']]                       # Voltage on front plate [V]
+    }
+    pdict['back_plate'] = {
+        "diam" : 3*pdict['in2m'], 
+        "hole_diam" : 3e-3,
+        "thickness" : 1.5e-3, 
+        "zpos" : pdict['size'][1]/2 - pdict['mount_zdims']["sample_holder"]/2,
+        "voltage" : - maxhva_settings[pdict['HVA']]
+    } 
+elif pdict['assembly'] == 2 : 
+    # Overall thickness was approximately .5 inches with a .125 inch lip on one end and .125 gap on the other end of sample.
+    # Once the sample was dropped into the mount with the surface hugging the PVC lip, it was held down with a nylon set screw (with a rubberized tip)
+    # This plate used is an aluminum rectangular plate (will incorporate cartesian coordinates into program soon)
+    pdict['HVA'] = "SVR350"
+    pdict['mount_zdims'] = {
+        "sample_holder" : .5*pdict['in2m']
+    }
+    if pdict['coords'] == 'cartesian':
+        pdict['front_plate'] = {
+            "diam" : 0.02794,                                 # diameter of plate [m]
+            "hole_diam" : 3e-3,                               # aperture diameter [m]
+            "thickness" : 1.27e-3, 
+            "zpos" : pdict['size'][1]/2 + pdict['mount_zdims']["sample_holder"]/2,    # location of plate surface (com) [m]
+            "voltage" : maxhva_settings[pdict['HVA']]/2                               # Voltage on front plate [V] (MAX value for associated HVA)
+        }
+        pdict['back_plate'] = {
+            "diam" : 0.02794,
+            "hole_diam" : 3e-3,
+            "thickness" : 1.27e-3, 
+            "zpos" : pdict['size'][1]/2 - pdict['mount_zdims']["sample_holder"]/2,
+            "voltage" : - maxhva_settings[pdict['HVA']]/2
+        }
+    elif pdict['coords'] == 'cylindrical':
+        pdict['front_plate'] = {
+            "diam" : 0.02794,                                 # diameter of plate [m]
+            "hole_diam" : 3e-3,                               # aperture diameter [m]
+            "thickness" : 1.27e-3, 
+            "zpos" : pdict['size'][1]/2 + pdict['mount_zdims']["sample_holder"]/2,    # location of plate surface (com) [m]
+            "voltage" : maxhva_settings[pdict['HVA']]/2                                  # Voltage on front plate [V]
+        }
+        pdict['back_plate'] = {
+            "diam" : 0.02794,
+            "hole_diam" : 3e-3,
+            "thickness" : 1.27e-3, 
+            "zpos" : pdict['size'][1]/2 - pdict['mount_zdims']["sample_holder"]/2,
+            "voltage" : maxhva_settings[pdict['HVA']]/2  
+        }
+        
+elif pdict['assembly'] == 3 :
+    #Set front and back plate params
+    pdict['HVA'] =  "TREK10/10B-HS"
+    pdict['mount_zdims'] = {
+        "total_zthickness" : 25.94e-3 ,                         # holds both sample and both electrodes [m]
+        "sample_holder" : 6.94e-3                              # width of lip that separates sample from electrodes [m] 
+    }
+    pdict['front_plate'] = {
+        "diam" : 31.5e-3,                                       # diameter of plate [m]
+        "hole_diam" : 3e-3,                                  # aperture diameter [m]
+        "thickness" : 9.66e-3, 
+        "zpos" : pdict['size'][1]/2 + (pdict['mount_zdims']["sample_holder"]/2),      # location of plate surface (com) [m]
+        "voltage" : maxhva_settings[pdict['HVA']]/2                                     # Voltage on front plate [V]
+    }
+    pdict['back_plate'] = {
+        "diam" : 31.5e-3,
+        "hole_diam" : 3e-3,
+        "thickness" : 9.66e-3, 
+        "zpos" : pdict['size'][1]/2 - (pdict['mount_zdims']["sample_holder"]/2),
+        "voltage" : -maxhva_settings[pdict['HVA']]/2
+    }
+    
+elif pdict['assembly'] == 4 :
+    #Set front and back plate params
+    pdict['mount_zdims'] = {
+        "total_zthickness" : 25.94e-3 ,                         # holds both sample and both electrodes [m]
+        "sample_holder" : 6.94e-3                              # width of lip that separates sample from electrodes [m] 
+    }
+    pdict['front_plate'] = {
+        "diam" : 31.5e-3,                                       # diameter of plate [m]
+        "hole_diam" : 3e-3,                                  # aperture diameter [m]
+        "thickness" : 9.66e-3, 
+        "zpos" : pdict['size'][1]/2 + (pdict['mount_zdims']["sample_holder"]/2),      # location of plate surface (com) [m]
+        "voltage" :  maxhva_settings[pdict['HVA']]/2                                      # Voltage on front plate [V]
+    }
+    pdict['back_plate'] = {
+        "diam" : 31.5e-3,
+        "hole_diam" : 3e-3,
+        "thickness" : 9.66e-3, 
+        "zpos" : pdict['size'][1]/2 - (pdict['mount_zdims']["sample_holder"]/2),
+        "voltage" : - maxhva_settings[pdict['HVA']]/2 
+    }
+    
+pdict['cap_params'] = {
+        "area" : np.pi*((pdict['front_plate']['diam']/2.0)**2), 
+        "d_air": (pdict['mount_zdims']['sample_holder']-pdict['optic']['thickness'])/2.0, 
+        "air_eps" : 1.0006
+    }
+
+pdict['cap_params']['cap_div_area'] = (pdict['optic']['sub_eps']*pdict['optic']['coat_eps']*pdict['cap_params']['air_eps'])/((2.0*pdict['optic']['sub_eps']*pdict['optic']['coat_eps']*pdict['cap_params']['d_air']) + (pdict['optic']['sub_eps']*pdict['cap_params']['air_eps']*pdict['optic']['coat_thickness']) + (pdict['optic']['coat_eps']*pdict['cap_params']['air_eps']*(pdict ['optic']['sub_thickness'])))
+pdict['cap_params']['capacitance'] = pdict['cap_params']['cap_div_area']*pdict['cap_params']['area']
+
+# system location params / metadata
+pdict['loc_params'] = {
+    'center of optic' : {
+        'cross_section_coord' : 'z',
+        'rho' : 0,
+        'z1_bound' : pdict['front_plate']['zpos'],
+        'z2_bound' : pdict['back_plate']['zpos']
+    },
+    'edge of hole' : {
+        'cross_section_coord' : 'z',
+        'rho' : pdict['front_plate']['hole_diam'],
+        'z1_bound' : pdict['front_plate']['zpos'],
+        'z2_bound' : pdict['back_plate']['zpos']
+    },
+    'edge of optic' : {
+        'cross_section_coord' : 'z',
+        'rho' : pdict['optic']['diam'] /2,
+        'z1_bound' : pdict['front_plate']['zpos'],
+        'z2_bound' : pdict['back_plate']['zpos']
+    },
+    'edge of plate' : {
+        'cross_section_coord' : 'z',
+        'rho' : pdict['front_plate']['diam']/2,
+        'z1_bound' : pdict['front_plate']['zpos'],
+        'z2_bound' : pdict['back_plate']['zpos']
+    },
+    'halfway out on optic' : {
+        'cross_section_coord' : 'z',
+        'rho' : pdict['optic']['diam']/4,
+        'z1_bound' : pdict['front_plate']['zpos'],
+        'z2_bound' : pdict['back_plate']['zpos']
+    },
+    'front of plate' : {
+        'cross_section_coord' : 'rho',
+        'rho1_bound' : 0,
+        'rho2_bound' : pdict['size'],
+        'z' : pdict['back_plate']['zpos']
+    },
+    'front of optic' : {
+        'cross_section_coord' : 'rho',
+        'rho1_bound' : 0,
+        'rho2_bound' : pdict['size'],
+        'z' : pdict['optic']['z_com'] + pdict['optic']['thickness']/2
+    },
+    'middle of optic' : {
+        'cross_section_coord' : 'rho',
+        'rho1_bound' : 0,
+        'rho2_bound' : pdict['size'],
+        'z' : pdict['optic']['z_com']
+    },
+    'back of optic' : {
+        'cross_section_coord' : 'rho',
+        'rho1_bound' : 0,
+        'rho2_bound' : pdict['size'],
+        'z' : pdict['optic']['z_com'] - pdict['optic']['thickness']/2
+    },
+    'back plate' : {
+        'cross_section_coord' : 'rho',
+        'rho1_bound' : 0,
+        'rho2_bound' : pdict['size'],
+        'z' : pdict['back_plate']['zpos']
+    }
+}
+
+
