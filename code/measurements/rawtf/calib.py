77ac64eda1ec8cfc5ed1c81b8c1acc2bd3209183
dancvan <dvande72hyde@hotmail.com>
2024-04-23 12:40:12 -0400

    Coupling transfer function as published but with a differing e-field conversion efficiency 13.3 [V/m]/[V]

--- code/measurements/rawtf/calib.py
+++ code/measurements/rawtf/calib.py
@@ -5,7 +5,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.style.use('ppt2latexsubfig')
+plt.style.use('ppt2latex2')
 plt.rcParams["font.family"] = "Times New Roman"
 
 def transfer_function(amplitude, phase,load_corr=False,anal_tag='sr785',\
@@ -50,16 +50,28 @@ home_dir = 'calib_tfs/'
 
 ## OLG
 G_dir = home_dir + 'OLG/'
-G_mag_data = np.loadtxt(G_dir + 'SCRN0530.TXT').transpose()
-G_phase_data = np.loadtxt(G_dir + 'SCRN0531.TXT').transpose()
-G = transfer_function(G_mag_data[1], G_phase_data[1])
+Gf_mag_data = np.loadtxt(G_dir + 'SCRN0530.TXT').transpose()
+Gf_phase_data = np.loadtxt(G_dir + 'SCRN0531.TXT').transpose()
+Gs_mag_data = np.loadtxt(G_dir + 'SCRN0534.TXT').transpose()
+Gs_phase_data = np.loadtxt(G_dir + 'SCRN0535.TXT').transpose()
+
+Gf = transfer_function(Gf_mag_data[1], Gf_phase_data[1])
+Gs = transfer_function(Gs_mag_data[1], Gs_phase_data[1])
 
 ## A1 (HVA CH3) -> in-loop
 A1_dir = home_dir + 'HVA_3ch/'
 A1_mag_data = np.loadtxt(A1_dir + 'SCRN0494.TXT').transpose()
 A1_phase_data = np.loadtxt(A1_dir + 'SCRN0495.TXT').transpose()
 A1 = transfer_function(A1_mag_data[1], A1_phase_data[1])
-A1
+
+## SR560 (Summing port)
+SR560_dir = home_dir + 'SR560/'
+SR560_mag_data = np.loadtxt(SR560_dir + 'SCRN0454.TXT').transpose()
+SR560_phase_data = np.loadtxt(SR560_dir + 'SCRN0455.TXT').transpose()
+SR560_mag2_data = np.loadtxt(SR560_dir + 'SCRN0456.TXT').transpose()
+SR560_phase2_data = np.loadtxt(SR560_dir + 'SCRN0457.TXT').transpose()
+SR560_tf1 = transfer_function(SR560_mag_data[1], SR560_phase_data[1])
+SR560_tf2 = transfer_function(SR560_mag2_data[1], SR560_phase2_data[1])
 
 ## A2 (HVA trek) -> electrodes
 A2_dir = home_dir + 'HVA_trek/'
@@ -68,17 +80,35 @@ A2_phase_data = np.loadtxt(A2_dir + 'trek_phase.TXT').transpose()
 A2 = transfer_function(A2_mag_data[1], A2_phase_data[1])
 
 # Compute coupling efficiency (C)
-C = lambda tf, E : (L_cav/nu_laser)*(tf*(1+G)*A1*v2hz)/(G*A2*E)
+C = lambda tf, G_, E : (L_cav/nu_laser)*(tf*(1-G_)*A1*(v2hz*SR560_tf1))/(G_*A2*E)
 
-C_fast = C(tf_fast, ef_eff)
-C_slow = C(tf_slow, ef_eff)
+C_fast = C(tf_fast, Gf, ef_eff)
+C_slow = C(tf_slow, Gs, ef_eff)
+diff = C_slow - C_fast
 
 ## Compute differential (between fast and slow axes)
+C_slow_mag = np.sqrt(C_slow*np.conj(C_slow))
+C_fast_mag = np.sqrt(C_fast*np.conj(C_fast))
+diff_mag = np.sqrt(diff*np.conj(diff))
 
-plt.loglog(tffastmag_data[0], np.abs(C_fast))
-plt.loglog(tffastmag_data[0], np.abs(C_slow))
-plt.loglog(tffastmag_data[0], np.abs(np.abs(C_slow)-np.abs(C_fast)), alpha=.5)
-plt.xlim([tffastmag_data[0][0], tffastmag_data[0][-1]])
+fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
+ax1.loglog(tffastmag_data[0], C_slow_mag, label = 'slow\_axis')
+ax1.loglog(tffastmag_data[0], C_fast_mag, label = 'fast\_axis')
+ax1.loglog(tffastmag_data[0], diff_mag, alpha=.5, label='differential')
+ax1.hlines(y=7e-17, xmin=20e3, xmax=40e3, linestyle='--', linewidth=5.0, color='m')
+ax1.tick_params(axis='y', which='minor')
+ax1.set_xlim(tffastmag_data[0][0], tffastmag_data[0][-1])
+ax1.set_ylabel('Coupling [[m]/[V/m]]')
+ax1.legend()
+ax2.semilogx(tffastmag_data[0], (np.arctan2(np.imag(C_slow), np.real(C_slow)))*(180/np.pi), label = 'slow\_axis')
+ax2.semilogx(tffastmag_data[0], (np.arctan2(np.imag(C_fast), np.real(C_fast)))*(180/np.pi), label = 'fast\_axis')
+ax2.semilogx(tffastmag_data[0], (np.arctan2(np.imag(diff), np.real(diff)))*(180/np.pi) , alpha=.5, label = 'differential')
+ax2.set_xlim(tffastmag_data[0][0], tffastmag_data[0][-1])
+ax2.set_ylim(-180.0, 180.0)
+ax2.legend()
+ax2.set_xlabel('Frequency [Hz]')
+ax2.set_ylabel('Phase [deg]')
+#plt.show()
 plt.savefig('../../../figs/ALGAAS/coupling_tf.pdf', dpi=300, format='pdf', bbox_inches='tight')
 
 
